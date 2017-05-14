b02b4566a72cc585b888cf037a8737f25246e839
sahagun <j-sahagun@live.com>
2017-05-14 11:48:35 -0700

    Done with send and receive, I think. I'm out for a snack break!

--- digital_sig/DigitalSignature.java
+++ digital_sig/DigitalSignature.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 
 /*
  * 			---- How to use DigitalSignature class ----
- * 
+ *
  * Basics:
  * 	+ DigitalSignature class provides static methods for:
  * 		- Creating a digital signature from an existing file
@@ -23,52 +23,51 @@ import java.util.ArrayList;
  * 	  and using keys).
  * 		- Signer uses their private key to sign.
  * 		- Receiver uses signer's public key to verify signature
- * 
+ *
  * How to create a Digital Signature:
- * 		// Assume you are signing a file named "blah.txt" and that you have your private key 
+ * 		// Assume you are signing a file named "blah.txt" and that you have your private key
  * 		// is in the file "myprivatekey.rsa"
  * 		RSAKey myPrivateKey = RSAKey.loadFromFile("myprivkey.rsa");
  * 		DigitalSignature.signFile("blah.txt", myPrivateKey);	// This creates a new file named "blah.txt.signed"
  * 																// that contains your digital signature.
- * 
+ *
  * How to verify a Digital Signature:
- * 		// Assume you are verifying a file named "blah.txt.signed" and that you have the signer's 
+ * 		// Assume you are verifying a file named "blah.txt.signed" and that you have the signer's
  * 		// public key in a file called "signerpublickey.rsa"
  * 		RSAKey signerPublicKey = RSAKey.loadFromFile("signerpublickey.rsa");
  * 		boolean isValid = DigitalSignature.verifySignature("blah.txt.signed", signerPublicKey);
- * 		
+ *
  */
 
 public class DigitalSignature {
 
 	public static void main(String[] args){
-		
+
 		RSAKey privateKey = RSAKey.loadFromFile("privkey.rsa");
 		signFile("message.txt", privateKey);
-		
+
 		RSAKey publicKey = RSAKey.loadFromFile("pubkey.rsa");
 		boolean valid = verifySignature("message.txt.signed", publicKey);
 		if(valid)
 			System.out.println("It's Valid!");
 		else
 			System.out.println("It's not valid :( ");
-		
 	}
-	
-	
+
+
 	public static boolean signFile(String filename, RSAKey privateKey){
-		
+
 		byte[] messageByteArray = fileToByteArray(filename);
 		if(messageByteArray == null){
 			System.out.println("Error: DigitalSignature.signFile(...) could not find/read file");
 			return false;
 		}
-		
-		
+
+
 		String messageString = new String(messageByteArray);
-		
+
 		byte[] digestArray;
-		
+
 		try{
 			MessageDigest digestor = MessageDigest.getInstance("MD5");
 			digestor.update(messageByteArray);
@@ -78,44 +77,44 @@ public class DigitalSignature {
 			System.out.println("Error: DigitalSignature.signFile(...) Problem using MD5 Message Digest");
 			return false;
 		}
-		
-		
+
+
 		BigInteger digest = new BigInteger(1, digestArray);
 		//System.out.println("Unencrypted Digest Before Signing:  " + digest);
 		BigInteger signedDigest = privateKey.encrypt(digest);
-		
+
 		if(!writeSignatureFile(filename + ".signed", signedDigest, messageString)){
 			System.out.println("Error: DigitalSignature.signFile(...) Could not write .signed file");
 			return false;
 		}
-		
+
 		return true;
 	}
-	
-	
 
-	
+
+
+
 	public static boolean verifySignature(String filename, RSAKey publicKey){
 		// !! Attention.. This method works correctly but needs updates !!
 		//		1) close ObjectInputStream after use
 		//		2) fill in exception handling sections
-		
-		
+
+
 		// Get BigInteger and byte array from signature file
 		BigInteger signatureBigInt = BigInteger.ONE;
 		ArrayList<Byte> byteArrayList = new ArrayList<>();;
 		byte[] messageByteArray = null;
-		
+
 		try{
 			FileInputStream fin = new FileInputStream(filename);
 			ObjectInputStream objectIn = new ObjectInputStream(fin);
-			
+
 			signatureBigInt = (BigInteger)objectIn.readObject();
 
 			while(true){
 				byteArrayList.add(objectIn.readByte());
 			}
-			
+
 		}
 		catch(EOFException e){
 			// This exception is expected when finished reading file
@@ -125,21 +124,21 @@ public class DigitalSignature {
 			}
 		}
 		catch(FileNotFoundException e){
-			
+
 		}
 		catch(IOException e){
-			
+
 		}
 		catch(ClassNotFoundException e){
-			
+
 		}
 
-		
+
 		// Convert BigInteger into unencrypted digest (digest1)
 		BigInteger decryptedDigestBigInt = publicKey.encrypt(signatureBigInt);
 		//System.out.println("Decrypted Digest:   " + decryptedDigestBigInt);
 		byte[] digest1 = decryptedDigestBigInt.toByteArray();
-		
+
 		// Convert byte array into unencrypted digest (digest2)
 		byte[] digest2 = null;
 		try{
@@ -150,15 +149,15 @@ public class DigitalSignature {
 		catch(NoSuchAlgorithmException e){
 			return false;
 		}
-		
+
 		BigInteger unencryptedDigestBigInt2 = new BigInteger(digest2);
 		//System.out.println("Unencrypted digest2:   " + unencryptedDigestBigInt2);
 
-		
+
 		// Compare digest1 and digest 2
 		if(digest1 == null || digest2 == null || digest1.length != digest2.length)
 			return false;
-		
+
 		//System.out.println("Digest1 length: " + digest1.length + "  Digest2 length: " + digest2.length);
 		boolean valid = true;
 		for(int i = 0; i < digest1.length; i++){
@@ -167,18 +166,18 @@ public class DigitalSignature {
 				valid = false;
 			}
 		}
-		
+
 		return valid;
 	}
-	
+
 	private static boolean writeSignatureFile(String filename, BigInteger signedDigest, String originalMessage){
 		try{
 			FileOutputStream fout = new FileOutputStream(filename);
 			ObjectOutputStream objectOut = new ObjectOutputStream(fout);
-			
+
 			objectOut.writeObject(signedDigest);
 			objectOut.write(originalMessage.getBytes());
-			
+
 			objectOut.close();
 		}
 		catch(FileNotFoundException e){
@@ -187,21 +186,21 @@ public class DigitalSignature {
 		catch(IOException e){
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	private static byte[] fileToByteArray(String filename){
 		ArrayList<Byte> byteArrayList = new ArrayList<>();
-		
+
 		try{
 			FileInputStream fin = new FileInputStream(filename);
-			
+
 			int inputInt = fin.read();
 			while(inputInt >= 0){
 				byte b = (byte) inputInt;
 				byteArrayList.add(b);
-				
+
 				inputInt = fin.read();
 			}
 			fin.close();
@@ -212,13 +211,13 @@ public class DigitalSignature {
 		catch(IOException e){
 			return null;
 		}
-		
+
 		byte[] byteArray = new byte[byteArrayList.size()];
 		for(int i = 0; i < byteArrayList.size(); i++){
 			byteArray[i] = (byte)byteArrayList.get(i);
 		}
-		
+
 		return byteArray;
 	}
-	
+
 }
--- digital_sig/KeyGen.java
+++ digital_sig/KeyGen.java
@@ -9,61 +9,61 @@ import java.security.SecureRandom;
  * 	Basics:
  * 	+ KeyGen objects are used to create public/private key pairs for RSA public key encryption.
  * 	+ For information about how to use RSAKey objects, see instructions in RSAKey.java
- * 
+ *
  * 	How to create a Key Generator:
  * 		KeyGen keyGen = new KeyGen();
- * 
+ *
  * 	How to generate a new public/private key pair:
  * 		keyGen.generate();
- * 
+ *
  * 	To get the generated keys from the KeyGen object, use these methods:
  * 		RSAKey publicKey = keyGen.getPublicKey();
  * 		RSAKey privateKey = keyGen.getPrivateKey();
- * 
+ *
  * 	A KeyGen object always remembers the last pair of keys that it generated.
- * 
+ *
  * 	A KeyGen object also remembers the data associated with creating those keys (p, q, n, totient, e, d). To
  * 	print out this data:
  * 		keyGen.print();
- * 
+ *
  * 	See the main() method for more examples of how to use a key generator and the keys it generates.
- * 
+ *
  */
 public class KeyGen {
-	
+
 	private Random rng = new SecureRandom();
-	
+
 	private BigInteger p = BigInteger.ZERO;
 	private BigInteger q = BigInteger.ZERO;
 	private BigInteger n = BigInteger.ZERO;
 	private BigInteger totient = BigInteger.ZERO;
 	private BigInteger e = BigInteger.ZERO;
 	private BigInteger d = BigInteger.ZERO;
-	
+
 	private RSAKey privateKey = new RSAKey(BigInteger.ZERO, BigInteger.ZERO);
 	private RSAKey publicKey = new RSAKey(BigInteger.ZERO, BigInteger.ZERO);
-	
+
 	public void generate(){
 		p = new BigInteger(512, 100, rng);
-		
+
 		do{
 			q = new BigInteger(512, 100, rng);
 		}while(q.compareTo(p) == 0);
-		 
+
 		n = p.multiply(q);
-		
+
 		totient = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-		
+
 		do{
 			e = new BigInteger(511, rng).setBit(512);
 		}while(e.gcd(totient).compareTo(BigInteger.ONE) != 0);
-		
+
 		d = e.modInverse(totient);
-		
+
 		privateKey = new RSAKey(d, n);
 		publicKey = new RSAKey(e, n);
 	}
-	
+
 	public void print(){
 		System.out.println();
 		System.out.println("============================================================");
@@ -78,16 +78,16 @@ public class KeyGen {
 		System.out.println("============================================================");
 		System.out.println();
 	}
-	
+
 	public RSAKey getPrivateKey(){
 		return privateKey;
 	}
-	
+
 	public RSAKey getPublicKey(){
 		return publicKey;
 	}
-	
-	
+
+
 	public static void main(String[] args){
 		KeyGen keygen = new KeyGen();
 		keygen.generate();
@@ -95,18 +95,16 @@ public class KeyGen {
 
 		keygen.getPrivateKey().saveToFile("privkey.rsa");
 		keygen.getPublicKey().saveToFile("pubkey.rsa");
-		
+
 		RSAKey privateKey = RSAKey.loadFromFile("privkey.rsa");
 		RSAKey publicKey = RSAKey.loadFromFile("pubkey.rsa");
-		
+
 		BigInteger originalMessage = new BigInteger("23012934765923085715630298653408872368778");
 		BigInteger encryptedMessage = privateKey.encrypt(originalMessage);
 		BigInteger decryptedMessage = publicKey.encrypt(encryptedMessage);
-		
+
 		System.out.println("Original Message:      " + originalMessage);
 		System.out.println("Encrypted Message:     " + encryptedMessage);
 		System.out.println("Decrypted Message:     " + decryptedMessage);
 	}
-	
-	
 }
