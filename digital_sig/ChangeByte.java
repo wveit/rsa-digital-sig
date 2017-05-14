ff378f891e9b4984fa0fc11c48035911454ab4a6
sahagun <j-sahagun@live.com>
2017-05-14 10:30:18 -0700

    created empty file that will be needed

---
+++ digital_sig/ChangeByte.java
@@ -0,0 +1,9 @@
+public class ChangeByte {
+/*
+a. Prompt the user for the input file name
+b. Open the file as a binary file
+c. Prompt the user for which byte to change (i.e. you are indexing the file byte by byte)
+d. Change the byte to a random value and close the file
+*/
+
+}
